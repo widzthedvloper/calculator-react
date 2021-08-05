bdb1ccad6507f8d852fa30f8211043dadcf02cff
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-05 17:56:36 -0400

    Convert AppComponent into a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Display from './Display';
@@ -5,18 +7,27 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 // I think i'm going to use the calculatorObject as a prop or state in the next milestone
 const calculatorObject = {
-  total: 0,
-  next: 0,
-  operation: '',
+  
 };
 
-function AppComponent() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default AppComponent;
