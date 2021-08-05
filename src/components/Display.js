955f72120da5025031ba7b2f1631b0c95447b03f
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-05 09:28:54 -0400

    Update Display components

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,24 +1,20 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import ButtonPanel from './ButtonPanel';
+import PropTypes from 'prop-types';
 
-class Display extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myState: 'I will display something here later!',
-    };
-  }
-
-  render() {
-    const { myState } = this.state;
-    return (
-      <div>
-        <p>{myState}</p>
-        <ButtonPanel />
-      </div>
-    );
-  }
+function Display({ prop }) {
+  return (
+    <div className="display">
+      {prop}
+    </div>
+  );
 }
+
+Display.propTypes = {
+  prop: PropTypes.string,
+};
+
+Display.defaultProps = {
+  prop: '0',
+};
 
 export default Display;
