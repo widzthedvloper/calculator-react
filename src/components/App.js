722b1ba72a07464ed862460ebf5d47ee18628185
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-05 09:14:07 -0400

    Remove div and add short fragment syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
-    <div>
+    <>
       <Display />
       <ButtonPanel />
-    </div>
+    </>
   );
 }
 
