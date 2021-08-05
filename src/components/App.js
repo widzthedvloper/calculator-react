72d5a45dbf533384cb55e9948786d2e7601d0868
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-05 15:14:34 -0400

    Import calculate

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
-
+// I think i'm going to use the calculatorObject as a prop or state in the next milestone
 const calculatorObject = {
   total: 0,
   next: 0,
