77ffd99e1023dae1cb77173221c5581ccc25c719
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-05 19:05:43 -0400

    Add function handleClick and pass it as prop

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,37 +1,39 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Button from './Button';
 
-function ButtonPanel() {
+function ButtonPanel({ clickHandler }) {
+  const handleClick = (buttonName) => clickHandler(buttonName);
   return (
     <div className="button-panel">
       <div className="Group1">
-        <Button prop="AC" />
-        <Button prop="+/-" />
-        <Button prop="%" />
-        <Button prop="÷" />
+        <Button prop="AC" clickHandler={handleClick} />
+        <Button prop="+/-" clickHandler={handleClick} />
+        <Button prop="%" clickHandler={handleClick} />
+        <Button prop="÷" clickHandler={handleClick} />
       </div>
       <div className="Group2">
-        <Button prop="7" />
-        <Button prop="8" />
-        <Button prop="9" />
-        <Button prop="x" />
+        <Button prop="7" clickHandler={handleClick} />
+        <Button prop="8" clickHandler={handleClick} />
+        <Button prop="9" clickHandler={handleClick} />
+        <Button prop="x" clickHandler={handleClick} />
       </div>
       <div className="Group3">
-        <Button prop="4" />
-        <Button prop="5" />
-        <Button prop="6" />
-        <Button prop="-" />
+        <Button prop="4" clickHandler={handleClick} />
+        <Button prop="5" clickHandler={handleClick} />
+        <Button prop="6" clickHandler={handleClick} />
+        <Button prop="-" clickHandler={handleClick} />
       </div>
       <div className="Group4">
-        <Button prop="1" />
-        <Button prop="2" />
-        <Button prop="3" />
-        <Button prop="+" />
+        <Button prop="1" clickHandler={handleClick} />
+        <Button prop="2" clickHandler={handleClick} />
+        <Button prop="3" clickHandler={handleClick} />
+        <Button prop="+" clickHandler={handleClick} />
       </div>
       <div className="Group5">
-        <Button prop="0" />
-        <Button prop="." />
-        <Button prop="=" />
+        <Button prop="0" clickHandler={handleClick} />
+        <Button prop="." clickHandler={handleClick} />
+        <Button prop="=" clickHandler={handleClick} />
       </div>
     </div>
   );
