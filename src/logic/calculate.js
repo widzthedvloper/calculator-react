380e4bc9a98642353d2a7a50f2ebaaaec1d690ff
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-05 15:15:59 -0400

    Implement calculate function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,21 +1,5 @@
+import operate from './operate';
 
 export default function calculate(calculatorObject, buttonName) {
-  switch (buttonName) {
-    case '+/-': plusOrMinus();
-      break;
-    case 'AC': allClear();
-      break;
-    case '%': reminder();
-      break;
-    case '÷': divide();
-      break;
-    case 'x': multiply();
-      break;
-    case '-': substract();
-      break;
-    case '+': add();
-      break;
-    case '=': result();
-    default: allClear();
-  }
+  operate(calculatorObject.total, calculatorObject.next, buttonName);
 }
