3c03b1189dc671ce65dc3f4c40a75dfc1bbb6d0f
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-06 10:56:49 -0400

    Handle zero cases

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,19 +1,35 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-param-reassign */
 import Big from 'big.js';
 
-export default function operate(numberOne, numberTwo, operation) {
-  let total = Big(numberOne);
-  let next = Big(numberTwo);
+export default function operate(numberOne = '0', numberTwo = '0', operation) {
+  let total = new Big(numberOne);
+  let next = new Big(numberTwo);
+  const zeroCase = (ope) => {
+    if ((ope === '÷' || ope === '%') && next === 0) {
+      total = undefined;
+    } else if (ope === '÷' && !next) {
+      total = total.div(next);
+    } else if (ope === '%' && !next) {
+      total = total.mod(next);
+    } else if (ope === '%' && numberTwo === 0) {
+      total = 0;
+    } else if (ope === '÷' && numberTwo === 0) {
+      total = 0;
+    }
+    return {
+      total,
+      next,
+      operation,
+    };
+  };
   switch (operation) {
-    case '+/-': total *= -1; next *= -1;
+    case '+/-': total = total.times(-1); next = next.times(-1);
       break;
     case 'AC': total = 0; next = 0;
       break;
-    case '%': total = total.mod(next);
-      break;
-    case '÷': total = total.div(next);
-      break;
+    case '%':
+      return zeroCase('%');
+    case '÷':
+      return zeroCase('÷');
     case 'x': total = total.times(next);
       break;
     case '-': total = total.minus(next);
