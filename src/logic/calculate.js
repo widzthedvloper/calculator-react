98991a79e711f2752d2c59f4eeba7f2cc670e0f6
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-06 08:15:56 -0400

    Return an object with the same keys as the state object

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,6 @@
 import operate from './operate';
 
 export default function calculate(calculatorObject, buttonName) {
-  operate(calculatorObject.total, calculatorObject.next, buttonName);
+  const myObject = operate(calculatorObject.total, calculatorObject.next, buttonName);
+  return myObject;
 }
