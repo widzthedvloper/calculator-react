b01f501294c6aed32017e5e77a73b8b9b035f398
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-13 17:21:21 -0400

    Fix linter problem

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,107 +1,107 @@
-import '@testing-library/jest-dom/extend-expect'
-import calculate from '../logic/calculate'
+import '@testing-library/jest-dom/extend-expect';
+import calculate from '../logic/calculate';
 
 const myState = {
-    total: null,
-    next: null,
-    operation: null,
-}
+  total: null,
+  next: null,
+  operation: null,
+};
 
 const resetState = () => {
-    myState.total = null;
-    myState.next = null;
-    myState.operation = null;
+  myState.total = null;
+  myState.next = null;
+  myState.operation = null;
 };
 
 it('Should return the the string 4 as the next propertie ', () => {
-    myState.total = '4';
-    myState.operation = '+';
-    const myResult = calculate(myState, '4');
-    expect(myResult.next).toEqual('4');
+  myState.total = '4';
+  myState.operation = '+';
+  const myResult = calculate(myState, '4');
+  expect(myResult.next).toEqual('4');
 });
 
 it('Should return the the string 8 as the total propertie ', () => {
-    myState.total = '4';
-    myState.next = '4';
-    myState.operation = '+';
-    const myResult = calculate(myState, '=');
-    expect(myResult.total).toEqual('8');
+  myState.total = '4';
+  myState.next = '4';
+  myState.operation = '+';
+  const myResult = calculate(myState, '=');
+  expect(myResult.total).toEqual('8');
 });
 
 it('Should return the string 4 as the total propertie ', () => {
-    const myResult = calculate(myState, '4');
-    expect(myResult.total).toEqual('4');
+  const myResult = calculate(myState, '4');
+  expect(myResult.total).toEqual('4');
 });
 
 it('Should return the string + as the operation propertie ', () => {
-    myState.total = '4'
-    const myResult = calculate(myState, '+');
-    expect(myResult.operation).toEqual('+');
+  myState.total = '4';
+  const myResult = calculate(myState, '+');
+  expect(myResult.operation).toEqual('+');
 });
 
 it('Should return the string -4 as the next propertie ', () => {
-    myState.total = '4'
-    myState.next = '4'
-    const myResult = calculate(myState, '+/-');
-    expect(myResult.next).toEqual(-4);
+  myState.total = '4';
+  myState.next = '4';
+  const myResult = calculate(myState, '+/-');
+  expect(myResult.next).toEqual(-4);
 });
 
 it('Should return the string -4 as the total propertie ', () => {
-    myState.total = '4'
-    myState.next = '4'
-    const myResult = calculate(myState, '+/-');
-    expect(myResult.total).toEqual(-4);
+  myState.total = '4';
+  myState.next = '4';
+  const myResult = calculate(myState, '+/-');
+  expect(myResult.total).toEqual(-4);
 });
 
 it('Should return the string - as the operation propertie ', () => {
-    resetState();
-    let myResult = calculate(myState, '4')
-    myResult = calculate(myState, '-');
-    expect(myResult.operation).toEqual('-');
+  resetState();
+  let myResult = calculate(myState, '4');
+  myResult = calculate(myState, '-');
+  expect(myResult.operation).toEqual('-');
 });
 
 it('Should return the string X as the operation propertie ', () => {
-    resetState();
-    let myResult = calculate(myState, '4')
-    myResult = calculate(myState, 'X');
-    expect(myResult.operation).toEqual('X');
+  resetState();
+  let myResult = calculate(myState, '4');
+  myResult = calculate(myState, 'X');
+  expect(myResult.operation).toEqual('X');
 });
 
 it('Should return the string ÷ as the operation propertie ', () => {
-    resetState();
-    let myResult = calculate(myState, '4')
-    myResult = calculate(myState, '÷');
-    expect(myResult.operation).toEqual('÷');
+  resetState();
+  let myResult = calculate(myState, '4');
+  myResult = calculate(myState, '÷');
+  expect(myResult.operation).toEqual('÷');
 });
 
 it('Should return the the string 8 as the total propertie ', () => {
-    myState.total = '12';
-    myState.next = '4';
-    myState.operation = '-';
-    const myResult = calculate(myState, '=');
-    expect(myResult.total).toEqual('8');
+  myState.total = '12';
+  myState.next = '4';
+  myState.operation = '-';
+  const myResult = calculate(myState, '=');
+  expect(myResult.total).toEqual('8');
 });
 
 it('Should return the the string 4 as the total propertie ', () => {
-    myState.total = '2';
-    myState.next = '2';
-    myState.operation = '+';
-    const myResult = calculate(myState, '=');
-    expect(myResult.total).toEqual('4');
+  myState.total = '2';
+  myState.next = '2';
+  myState.operation = '+';
+  const myResult = calculate(myState, '=');
+  expect(myResult.total).toEqual('4');
 });
 
 it('Should return the the string 2 as the total propertie ', () => {
-    myState.total = '6';
-    myState.next = '4';
-    myState.operation = '-';
-    const myResult = calculate(myState, '=');
-    expect(myResult.total).toEqual('2');
+  myState.total = '6';
+  myState.next = '4';
+  myState.operation = '-';
+  const myResult = calculate(myState, '=');
+  expect(myResult.total).toEqual('2');
 });
 
 it('Should return the the string 4 as the total propertie ', () => {
-    myState.total = '6';
-    myState.next = '2';
-    myState.operation = '÷';
-    const myResult = calculate(myState, '=');
-    expect(myResult.total).toEqual('3');
+  myState.total = '6';
+  myState.next = '2';
+  myState.operation = '÷';
+  const myResult = calculate(myState, '=');
+  expect(myResult.total).toEqual('3');
 });
